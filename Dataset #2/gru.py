f2de7e725663a1b227fc01c5633049a1840ddb18
salih997 <56520923+salih997@users.noreply.github.com>
2022-11-27 17:26:48 +0300

    data1 and data2 results
    Evualiation criteria are obtained

--- Dataset #2/gru.py
+++ Dataset #2/gru.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as o
 from matplotlib import pyplot as plt
+import time
 
 
 # Params
@@ -44,30 +45,37 @@ def take_data(input_path):
     return data, labels
 
 
+def train(X, Y, model, optimizer, loss_function, device, epoch=50):
 
-def train(X, Y, model, optimizer, loss_function, epoch=50):
-
+    start_time = time.process_time()
     for e in range(epoch):
         for i, data in enumerate(X):
-            prediction = model(data.unsqueeze(0))
-            loss = loss_function(prediction, Y[i])
+            prediction = model(data.unsqueeze(0).to(device))
+            loss = loss_function(prediction, Y[i].to(device))
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+        if e % 10 == 0:
+            print("Epoch", e, loss.detach())
+    end_time = time.process_time()
+    print("Training Time: ", end_time - start_time)
 
-        print("Epoch", e, loss.detach())
-    
-    return model
+    return model, (end_time - start_time)
 
 
-def test(X, Y, model):
+def test(X, Y, model, device):
+
+    start_time = time.process_time()
     correct = 0
     for i, data in enumerate(X):
-        prediction = model(data.unsqueeze(0))
+        prediction = model(data.unsqueeze(0).to(device))
         if torch.argmax(prediction.detach()) == Y[i]:
             correct += 1
-
+    end_time = time.process_time()
+    print("Test Time: ", end_time - start_time)
     print("Accuracy", correct/X.size()[0])
+
+    return (correct/X.size()[0]), (end_time - start_time)
 
 
 if __name__ == "__main__":
@@ -87,59 +95,47 @@ if __name__ == "__main__":
     #     plt.plot(range(len(data)), data, c=colormap[test_labels[i][0]])
     # plt.show()
 
-    m = GRU()
-    optim = o.Adam(m.parameters(), lr=0.001)
-    lf = nn.CrossEntropyLoss()
-    m = train(train_data, train_labels, m, optim, lf, epoch=100)
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
 
-    test(train_data, train_labels, m)
-    test(test_data, test_labels, m)
+    training_time_list = []
+    train_accuracy_list = []
+    train_set_testing_time_list = []
+    test_accuracy_list = []
+    test_set_testing_time_list = []
 
+    for i in range(10):         # 10 runs
+        print("Run", i+1)
+        print("-----")
 
+        m = GRU()
+        m.to(device)
+        
+        optim = o.Adam(m.parameters(), lr=0.001)
+        lf = nn.CrossEntropyLoss()
+        m, training_time = train(train_data, train_labels, m, optim, lf, device, epoch=100)
+        training_time_list.append(training_time)
 
+        train_acc, train_set_testing_time = test(train_data, train_labels, m, device)
+        train_accuracy_list.append(train_acc)
+        train_set_testing_time_list.append(train_set_testing_time)
 
+        test_acc, test_set_testing_time = test(test_data, test_labels, m, device)
+        test_accuracy_list.append(test_acc)
+        test_set_testing_time_list.append(test_set_testing_time)
 
+        print()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # plt.figure(1)
-    # colormap = ['r', 'g', 'b']
-    # for i, data in enumerate(train_data):
-    #     plt.plot(range(len(data)), data, c=colormap[train_labels[i][0]])
-
-    # plt.figure(2)
-    # colormap = ['r', 'g', 'b']
-    # for i, data in enumerate(test_data):
-    #     plt.plot(range(len(data)), data, c=colormap[test_labels[i][0]])
-    # plt.show()
-
-
+    print("Statistics:")
+    print("Average Training Time                ----->", sum(training_time_list) / len(training_time_list))
+    print("Average Training Accuracy            ----->", sum(train_accuracy_list) / len(train_accuracy_list))
+    print("Maximum Training Accuracy            ----->", max(train_accuracy_list))
+    print("Minimum Training Accuracy            ----->", min(train_accuracy_list))
+    print("Average Testing Time of Training Set ----->", sum(train_set_testing_time_list) / len(train_set_testing_time_list))
+    print("Average Testing Accuracy             ----->", sum(test_accuracy_list) / len(test_accuracy_list))
+    print("Maximum Testing Accuracy             ----->", max(test_accuracy_list))
+    print("Minimum Testing Accuracy             ----->", min(test_accuracy_list))
+    print("Average Testing Time of Test Set     ----->", sum(test_set_testing_time_list) / len(test_set_testing_time_list))
 
