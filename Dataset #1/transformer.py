3feb5eed55039e1107b069e848717925f503f4d3
salih997 <56520923+salih997@users.noreply.github.com>
2022-12-19 01:52:45 +0300

    data1 transformer bug fix
    problem of transformer is solved by adding positional encoding and changing number of layers

--- Dataset #1/transformer.py
+++ Dataset #1/transformer.py
@@ -1,32 +1,69 @@
 import torch
 import torch.nn as nn
 import torch.optim as o
-import pandas as pd
+import math
+from torch import Tensor
 
 
-# Params
+# Constant Params
 number_of_features = 1      # input_size
 number_of_classes = 3       # hidden_size
+sequence_length = 15
+
+# Hyperparameters
 number_of_layers = 1        # num_layers
+dropout = float(0.1)
+pos_encode_dimension = 10   # even number
+
 
 # batch_first = True
 # batch - sequence - feature    => input shape
 # batch - sequence - number of classes    => output shape
+
+## TODO 
+## 1+ Encoder layer'i project ederken num feature'i duzgun project et
+## 1- Add cuda to transformer model
+## 2- Mnist'i test datasi olarak kullan sanity check gibi 28*28lik dataseti 28 feature, 28 sequence length gibi kullan
+## 3- Multivariate olanlarin sonuclarini al
+## 4- Modellerin icini iyi anla
+
+# Note:
+# nheads must divide evenly into d_model (feature dimension)
 
 
 class Transformer(nn.Module):
 
     def __init__(self):
         super(Transformer, self).__init__()
-        self.layers = nn.TransformerEncoderLayer(d_model=number_of_features, nhead=1, batch_first=True)
-        self.transformer = nn.TransformerEncoder(self.layers, num_layers=6)
-        self.decoder = nn.Linear(number_of_features, number_of_classes)
+        self.pos_encoder = PositionalEncoding(pos_encode_dimension, dropout, sequence_length)
+        self.layers = nn.TransformerEncoderLayer(d_model=pos_encode_dimension, nhead=1, batch_first=True)
+        self.transformer = nn.TransformerEncoder(self.layers, num_layers=number_of_layers)
+        self.decoder = nn.Linear(pos_encode_dimension, number_of_classes)
 
 
     def forward(self, X):
+        X = self.pos_encoder(X)
         encoder_output = self.transformer(X)
         output = self.decoder(encoder_output[:, -1, :])
         return output
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 500):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).reshape((1, max_len, 1))
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))        
+        pe = torch.zeros(1, max_len, d_model)
+        pe[0, :, 0::2] = torch.sin(position * div_term)   
+        pe[0, :, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = x + self.pe
+        return self.dropout(x)
 
 
 def take_data(input_path):
@@ -42,18 +79,12 @@ def take_data(input_path):
         for s, split in enumerate(splitted):
             data[d, s] = float(split)
 
+    # Shuffle data
+    dd= torch.randperm(data.size()[0])
+    data = data[dd]
+
     labels = (data[:, 0].long() - 1).reshape(data.size()[0], 1)
     data = data[:, 1:16].float().reshape((data.size()[0], 15, 1))
-
-    return data, labels
-
-
-def take_data_tsv(input_path):
-    data = pd.read_csv(input_path, header=None, index_col=False, sep='\t')
-    data = torch.tensor(data.values)
-
-    labels = (data[:, 0].long() - 1).reshape(data.size()[0], 1)
-    data = data[:, 1:].float().reshape((data.size()[0], 176, 1))
 
     return data, labels
 
@@ -61,14 +92,16 @@ def take_data_tsv(input_path):
 def train(X, Y, model, optimizer, loss_function, epoch=50):
 
     for e in range(epoch):
+        current_loss = 0
         for i, data in enumerate(X):
             prediction = model(data.unsqueeze(0))
             loss = loss_function(prediction, Y[i])
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-
-        print("Epoch", e, loss.detach())
+            current_loss = current_loss + loss.item()
+        
+        print("Epoch", e, "LOSS TOTAL", current_loss)
     
     return model
 
@@ -84,18 +117,15 @@ def test(X, Y, model):
 
 
 if __name__ == "__main__":
-    
+
     train_data, train_labels = take_data("train_data.txt")
     test_data, test_labels = take_data("test_data.txt")
 
     m = Transformer()
     optim = o.Adam(m.parameters(), lr=0.001)                           ## try SGD
     lf = nn.CrossEntropyLoss()
-    m = train(train_data, train_labels, m, optim, lf, epoch=400)
+    m = train(train_data, train_labels, m, optim, lf, epoch=100)
 
     test(train_data, train_labels, m)
     test(test_data, test_labels, m)
-
-
-
 
