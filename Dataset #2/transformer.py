9221cee9458c4b9f18d36a43996efebd83db44d4
salih997 <56520923+salih997@users.noreply.github.com>
2022-12-24 20:29:10 +0300

    data2-parameter fix
    fixed transformer parameters

--- Dataset #2/transformer.py
+++ Dataset #2/transformer.py
@@ -13,7 +13,7 @@ number_of_classes = 6       # hidden_size
 sequence_length = 60
 
 # Hyperparameters
-number_of_layers = 1        # num_layers
+number_of_layers = 2        # num_layers
 dropout = float(0.1)
 pos_encode_dimension = 10   # even number
 
@@ -163,7 +163,7 @@ if __name__ == "__main__":
 
         optim = o.Adam(m.parameters(), lr=0.001)
         lf = nn.CrossEntropyLoss()
-        m, training_time = train(train_data, train_labels, m, optim, lf, device, epoch=100)
+        m, training_time = train(train_data, train_labels, m, optim, lf, device, epoch=150)
         training_time_list.append(training_time)
 
         train_acc, train_set_testing_time = test(train_data, train_labels, m, device)
